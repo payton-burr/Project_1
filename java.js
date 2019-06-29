1d18e831f31ba1651b4ea7ec3ce9487f171bcd2d
Bryce Senninger <bryce.senninger@gmail.com>
2019-06-29 09:15:12 -0600

    javascript functionality for adding rows and info dynamically

---
+++ java.js
@@ -0,0 +1,29 @@
+let description = "";
+let searchLocation = "";
+
+function runLocationSearch() {
+    $("#search-button1").click(function() {
+        $("#searchResults").empty();
+        let searchLocation = $("input").val().trim();
+        let innerHTML = '';
+
+        let queryURL = "https://cors-anywhere.herokuapp.com/" + "https://jobs.github.com/positions.json?description=" + description + "&location=" + searchLocation;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            let results = response;
+            for(var i = 0; i < results.length; i++){
+                innerHTML += '<tr>';
+                innerHTML += '<td>' + results[i].title + '</td>';
+                innerHTML += '<td>' + results[i].company + '</td>';
+                innerHTML += '<td>' + results[i].location + '</td>';
+                innerHTML += '<td><button id="infoButton">More Info</button></td>'
+                innerHTML += '</tr>';
+            }
+            $("#searchResults").append(innerHTML);
+        });
+    });
+}
+
+runLocationSearch();
